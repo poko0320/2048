8f986aff0f0b00d73bdb0a99a44e8bca5d7c24c7
JackH <jackhui322@gmail.com>
2024-12-20 20:14:30 +0800

    fix: test_game print score

--- testfile/test_game.c
+++ testfile/test_game.c
@@ -3,12 +3,12 @@
 #include "../include/lobby.h"
 #include "../include/game.h"
 
-
 int main() {
     Matrix *game = createMatrix(4, 4);
     setElement(game, 0, 0, 2);
     setElement(game, 0, 1, 2);
     moveAction(game, LEFT);
     printMap(game);
+    printf("%d\n", *game->score);
     return 0;
 }
