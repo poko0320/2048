4cd7e9403cd35c132bb78ce7303f53e796de434e
poko0320 <poko20050320@gmail.com>
2024-12-21 21:57:17 +1100

    add the main logic for game loop

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include "matrix.h"
+#include "../include/matrix.h"
+#include "../include/lobby.h"
+#include "../include/game.h"
 
 
 int main() {
-
+    printOpening();
+    Matrix *game = createMatrix(4, 4);
+    randomGenerate(game);
+    printMap(game);
+    input(game);
+    
+    while((!Is2048(game)) && IsMoveable(game)){
+        randomGenerate(game);
+        printMap(game);
+        input(game);
+    }
+    printf("END");
     return 0;
 }
